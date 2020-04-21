2cabb4694e59c1440a8d14c8ea7dff9c57b07e32
EduardMac <latcan.eduard@alexnel.com>
2020-04-21 07:21:22 +0300

    End of section 3 for all

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext conetxt, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!conetxt.ProductBrands.Any())
+                {
+                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                    foreach (var item in brands)
+                    {
+                        conetxt.ProductBrands.Add(item);
+                    }
+                    await conetxt.SaveChangesAsync();
+                }
+                if (!conetxt.ProductTypes.Any())
+                {
+                    var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+                    foreach (var item in types)
+                    {
+                        conetxt.ProductTypes.Add(item);
+                    }
+                    await conetxt.SaveChangesAsync();
+                }
+                if (!conetxt.Products.Any())
+                {
+                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                    foreach (var item in products)
+                    {
+                        conetxt.Products.Add(item);
+                    }
+                    await conetxt.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex.Message);
+            }
+        }
+    }
+}
